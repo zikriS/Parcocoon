12a8f0e754452059fe22f053200679269d325f0c
Lab Unama <senjutsudimal@gmail.com>
2022-07-25 17:20:47 +0700

    Test -> RV : Error Layout + Belum Implementasi Handler Pesan + Send Msg Done

--- app/src/main/java/com/example/projectkpbaru/DiskusiFragment.kt
+++ app/src/main/java/com/example/projectkpbaru/DiskusiFragment.kt
@@ -17,6 +17,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class DiskusiFragment : Fragment(R.layout.fragment_diskusi) {
     private lateinit var binding: FragmentDiskusiBinding
@@ -46,41 +47,30 @@ class DiskusiFragment : Fragment(R.layout.fragment_diskusi) {
 
 
         getMessage(adapter, msgLIst)
+        updateMessage(adapter, msgLIst)
     }
+
 
     private fun getMessage(adapter: ChatAdapter, msgLIst: MutableList<Message>) {
         Firebase.database.getReference("chatData")
-            .addValueEventListener(object : ValueEventListener {
+            .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     if (snapshot.exists()) {
-                        if (msgLIst.isEmpty()) {
-                            for (dataSnapshot in snapshot.children) {
-                                msgLIst.add(
-                                    Message(
-                                        dataSnapshot.child("uid").value.toString(),
-                                        dataSnapshot.child("userName").value.toString(),
-                                        dataSnapshot.child("message").value.toString()
-                                    )
+                        for(data in snapshot.children) {
+                            msgLIst.add(
+                                Message(
+                                    data.child("uid").value as String,
+                                    data.child("userName").value as String,
+                                    data.child("message").value as String
                                 )
-                                adapter.notifyItemInserted(msgLIst.size - 1)
-                            }
-                        } else {
-                            with(snapshot.children.last()) {
-                                msgLIst.add(
-                                    Message(
-                                        snapshot.child("uid").value as String,
-                                        snapshot.child("userName").value as String,
-                                        snapshot.child("message").value as String
-                                    )
-                                )
-                                adapter.notifyItemInserted(msgLIst.size - 1)
-                            }
+                            )
+                            adapter.notifyItemInserted(msgLIst.size-1)
                         }
                     }
                 }
 
                 override fun onCancelled(error: DatabaseError) {
-                    Log.w(TAG, "onCancelled: Cancelled Listener")
+                    Log.w(TAG, "onCancelled: Error")
                 }
             })
     }
@@ -91,12 +81,13 @@ class DiskusiFragment : Fragment(R.layout.fragment_diskusi) {
             userName = username,
             message = message
         )
-        Firebase.database.getReference("chatData").push().setValue(msg)
-            .addOnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Toast.makeText(requireContext(), "Pesan gagal dikirim", Toast.LENGTH_SHORT)
-                        .show()
-                }
+        binding.TvInputPesan.text.clear()
+        Firebase.database.getReference("chatData").push().setValue(msg).addOnCompleteListener{
+            if(it.isSuccessful) {
+                Toast.makeText(requireContext(), "Pesan Berhasil dikirim", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Pesan Gagal dikirim", Toast.LENGTH_SHORT).show()
             }
+        }
     }
 }
